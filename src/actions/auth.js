9660922bfbb0c1f539a0c8233867893e3ed0370e
Dagmawi Yehenew <dagmawi.yehenew@beet.se>
2022-11-29 20:55:31 +0100

    Travsport: localstore credential for now

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -5,6 +5,7 @@ export const Authenticator = ( async (credential) => {
             if(res) {
                 setAuthorizationToken(res.data.token);
                 localStorage.setItem('token', res.data.token);
+                localStorage.setItem('credential', JSON.stringify(credential));
             }
             return res;
     }).catch((error) =>{
